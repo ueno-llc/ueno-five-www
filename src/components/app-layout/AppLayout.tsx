d0772a17b01c33b37bc2cd86337a4715f8f8b9fa
Bjarni Freyr Bjornsson <bjarni@ueno.co>
2019-02-05 12:32:08 +0000

    Added meta data.

--- src/components/app-layout/AppLayout.tsx
+++ src/components/app-layout/AppLayout.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Helmet from 'react-helmet';
+import shareImage from 'assets/images/share.jpg';
 
 import { Devtools } from 'components/devtools/Devtools';
 
@@ -9,13 +10,26 @@ interface IProps {
   children: React.ReactNode;
 }
 
+const seoTitle = 'Tory Satins';
+const seoDescription = 'Tory Satins - The flower of my youth.';
+
 export default ({ children }: IProps) => (
   <div className={s.layout}>
     <Helmet
-      title="Ueno starter"
+      title="Tory Satins"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { property: 'og:title', content: seoTitle },
+        { name: 'twitter:title', content: seoTitle },
+        { name: 'description', content: seoDescription },
+        { name: 'twitter:description', content: seoDescription },
+        { name: 'keywords', content: 'Tory Satins, ueno' },
+        { property: 'og:image', content: shareImage },
+        { property: 'og:image:width', content: '1200px' },
+        { property: 'og:image:height', content: '630px' },
+        { property: 'og:image:alt', content: 'Troy Satins with guitar' },
+        { name: 'twitter:image', content: shareImage },
+        { name: 'twitter:card', content: 'summary' },
+        { name: 'twitter:site', content: '@uenodotco' },
       ]}
     >
       <html lang="en" />
